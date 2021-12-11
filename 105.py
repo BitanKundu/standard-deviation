93fd82094a7497c24f35d10a64f3c51d83234568
BitanKundu <79818809+BitanKundu@users.noreply.github.com>
2021-12-10 20:40:56 -0800

    Add files via upload

---
+++ 105.py
@@ -0,0 +1,18 @@
+import statistics
+x= [20,30,40,50]
+sum=0
+for i in x:
+    sum=sum+i
+print (sum)
+print(sum/len(x))
+
+print(statistics.mean(x))    
+
+print(statistics.mode(x)
+arr=[70,80,90,70,70]
+sum=0   
+for i in arr:
+    sum=sum+i`
+print(sum)
+print(sum/len(arr)) 
+print(statistics.mean(arr))   
